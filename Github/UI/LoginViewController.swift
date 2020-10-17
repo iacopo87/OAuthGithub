03054edc930588feabdae1926ff8d93c924e1b8a
Iacopo Pazzaglia <iacopo.p.es@sync.money>
2020-10-17 14:42:24 +0200

    feat: create LoginViewController

---
+++ Github/UI/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Github
+//
+//  Created by Iacopo Pazzaglia on 17/10/2020.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func onLoginClick(_ sender: Any) {
+    }
+}
